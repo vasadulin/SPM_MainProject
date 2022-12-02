740670b84b9dcfe9f43aa57833349e59bc10c160
Asadulin Viktor <vasadulin@gmail.com>
2022-12-02 19:29:10 +0200

    Initial Commit

---
+++ SPM_MainProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPM_MainProject
+//
+//  Created by vasadulin on 02.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
