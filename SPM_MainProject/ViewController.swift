a3cc56982da5604767159bdb4665ab404e94c9d8
Asadulin Viktor <vasadulin@gmail.com>
2022-12-05 20:51:11 +0200

    Open ViewController from remote library MyUILibrary

--- SPM_MainProject/ViewController.swift
+++ SPM_MainProject/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import CommonLibrary
+import MyUILibraty
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    let v = CommonLibrary()
+    v.printHello()
   }
 
-
+  @IBAction func openButtonAction(_ sender: Any) {
+    //Open ViewController from REMOTE library MyUILibrary
+    let vc = MainViewController.viewControllerFromStoryboard
+    self.present(vc, animated: true, completion: nil)
+  }
+  
+  @IBAction func openLocalButtonAction(_ sender: Any) {
+    //Open ViewController from LOCAL library MyUILibrary
+//    let vc = MainViewController.viewControllerFromStoryboard
+//    self.present(vc, animated: true, completion: nil)
+  }
 }
 
